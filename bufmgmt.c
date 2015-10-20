57e238de78f873a61c5afe6657fac4b3d08f991d
Benito Michelon <benito@Benitos-MacBook-Pro.local>
2015-10-20 00:38:30 -0200

    Finalmente a gerencia de buffers e a configuração estão prontas
    Signed-off-by: Benito Michelon <benito@Benitos-MacBook-Pro.local>

--- bufmgmt.c
+++ bufmgmt.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <assert.h>
+#include <glib.h>
 
 #define FILESIZE 268435456 // 256 * 1024 * 1024
 #define DATABLOCK 4096
-#define CONTROL DATABLOCK * 2
+#define CONTROLSIZE DATABLOCK * 3
+#define DATAFILE ".datafile"
 
 typedef struct Buffer {
 	int id;
@@ -19,11 +22,13 @@ int framesLen = 0;
 int vitima = 0;
 int miss = 0;
 int hit = 0;
+GList *free_blocks = NULL;
 
 Buffer *get_datablock(int id) {
 	int i;
+	FILE *fd;
 
-	printf("%s(%d)\n", __func__, id);
+	//printf("%s(%d)\n", __func__, id);
 	// Caso esteja nos frames (cache hit)
 	for (i = 0; i < framesLen; i++) {
 		if (frames[i].id == id) {
@@ -36,24 +41,25 @@ Buffer *get_datablock(int id) {
 	}
 	// Cache miss
 	miss++;
-	printf("Cache miss\n");
+	//printf("Cache miss\n");
 
 	// Caso NÃO esteja nos frames e o framebuffer ainda não estiver cheio
-	FILE *fd = fopen(".datafile", "r");
-	if (!fd)
-		printf("Erro ao abrir .datafile\n"), exit(1);
-
 	if (framesLen < 256) {
-		printf("Frames ainda nao ta cheio\n");
+		fd = fopen(DATAFILE, "r");
+		if (!fd)
+			printf("Erro ao abrir .datafile\n"), exit(1);
+
+		//printf("Frames ainda nao ta cheio\n");
 		frames[framesLen].datablock = malloc(DATABLOCK);
 		frames[framesLen].id = id;
 		frames[framesLen].dirty = 0;
 		frames[framesLen].used = 0;
-		if(fseek(fd, CONTROL + id * DATABLOCK, SEEK_SET) < 0)
-			perror("fseek");
-		if(fread(frames[framesLen].datablock, DATABLOCK, 1, fd) < 1)
-			perror("fudeu"), printf("Erro lendo datablock %d\n", id), exit(1);
 
+		if (fseek(fd, CONTROLSIZE + id * DATABLOCK, SEEK_SET) < 0)
+			perror("fseek");
+		if (fread(frames[framesLen].datablock, 1, DATABLOCK, fd) < DATABLOCK) {
+			perror("fudeu"), printf("Erro lendo datablock %d\n", id), exit(1);
+		}
 		fclose(fd);
 		return &frames[framesLen++];
 	}
@@ -63,17 +69,24 @@ Buffer *get_datablock(int id) {
 	while (frames[vitima].used)
 		frames[vitima++].used = 0;
 
+	printf("Vitima é o frame %d - id = %d\n", vitima, frames[vitima].id);
 	// Se buffer tiver sido alterado, salva ele (write back policy)
 	if (frames[vitima].dirty) {
-		fseek(fd, CONTROL + frames[vitima].id * DATABLOCK, SEEK_SET);
-		if (fwrite(frames[vitima].datablock, DATABLOCK, 1, fd) < DATABLOCK)
-			printf("Erro salvando datablock %d\n", frames[vitima].id), exit(1);
+		fd = fopen(DATAFILE, "r+");
+		assert(!fseek(fd, CONTROLSIZE + frames[vitima].id * DATABLOCK, SEEK_SET));
+		printf("writing on %zd\n", ftell(fd));
+		if (fwrite(frames[vitima].datablock, 1, DATABLOCK, fd) < DATABLOCK)
+			printf("Erro salvando datablock %d\n", frames[vitima].id), perror("qq deu?"), exit(1);
+		fclose(fd);
+		fd = NULL;
 	}
 
+	fd = fopen(DATAFILE, "r");
+	assert(fd);
 	// Lê o buffer do arquivo
-	fseek(fd, CONTROL + id * DATABLOCK, SEEK_SET);
+	assert(!fseek(fd, CONTROLSIZE + id * DATABLOCK, SEEK_SET));
 	if(fread(frames[vitima].datablock, 1, DATABLOCK, fd) < DATABLOCK)
-		printf("Erro lendo datablock %d\n", id), exit(1);
+		printf("Erro lendo datablock %d\n", id), perror("qq deu?"), exit(1);
 
 	// Configura estrutura de controle do datablock (Buffer)
 	frames[vitima].id = id;
@@ -83,21 +96,6 @@ Buffer *get_datablock(int id) {
 	fclose(fd);
 	return &frames[vitima++];
 };
-
-void bufmgmt_persist() {
-	int i;
-	FILE *fd = fopen(".datafile", "w");
-
-	for (i = 0; i < framesLen; i++) {
-		if (frames[i].dirty) {
-			fseek(fd, CONTROL + i * DATABLOCK, SEEK_SET);
-			if (fwrite(frames[i].datablock, 1, DATABLOCK, fd) < DATABLOCK)
-				printf("Erro salvando datablock %d\n", frames[i].id), exit(1);
-		}
-	}
-
-	fclose(fd);
-}
 
 typedef struct {
 	int header_len;
@@ -113,66 +111,107 @@ typedef struct {
 } EntryHeader;
 
 typedef struct {
-	char bitmap[268435456 / DATABLOCK / 8];
+	unsigned char bitmap[FILESIZE / DATABLOCK / 8];
 	int root;
 	int table;
 } Config;
 
-void init_database() {
+Config conf;
+
+void persist() {
+	int i;
+	FILE *fd = fopen(DATAFILE, "r+");
+
+	fwrite (&conf, sizeof(Config), 1, fd);
+
+	for (i = 0; i < framesLen; i++) {
+		if (frames[i].dirty) {
+			fseek(fd, CONTROLSIZE + i * DATABLOCK, SEEK_SET);
+//			printf("writing on %zd\n", ftell(fd));
+			if (fwrite(frames[i].datablock, 1, DATABLOCK, fd) < DATABLOCK)
+				printf("Erro salvando datablock %d\n", frames[i].id), exit(1);
+		}
+	}
+
+	fclose(fd);
+}
+
+
+void create_database() {
 	FILE *fd;
 	DBHeader *dbh;
 	char buf[DATABLOCK];
-	Config *conf;
 
-	fd = fopen(".datafile", "w");
+	fd = fopen(DATAFILE, "w");
 	if (ftruncate(fileno(fd), FILESIZE) < 0)
 		perror("ftruncate");
 	fclose(fd);
 
-	fd = fopen(".datafile", "w");
+	fd = fopen(DATAFILE, "r+");
 	if (!fd)
 		perror("Criando datafile"), exit(1);
-/*
-	// Escrevendo configuração
-	conf = malloc(sizeof(Config));
-	bzero(conf, sizeof(Config));
-	conf->root = 666;
-	printf("rooot = %d\n", conf->root);
-	fwrite(conf, sizeof(Config), 1, fd);
-*/
-	int val = 666;
-	fwrite(&val, 1, sizeof(int), fd);
-	puts("asdfasdfasdf");
 
-	fseek(fd, DATABLOCK * 2, SEEK_SET);
-	for (int i = 2; i < FILESIZE / DATABLOCK; i++) {
+	// Escrevendo configuração
+	bzero(&conf, sizeof(Config));
+	conf.root = 666;
+	fwrite(&conf, sizeof(Config), 1, fd);
+	fclose(fd);
+/*
+	for (int i = 3; i < FILESIZE / DATABLOCK; i++) {
+		fd = fopen(DATAFILE, "w");
+		assert(fd);
+		fseek(fd, CONTROLSIZE + i * DATABLOCK, SEEK_SET);
 		dbh = &buf[DATABLOCK - 1] - sizeof(DBHeader);
 		dbh->header_len = 666;
 		dbh->free = DATABLOCK - sizeof(DBHeader) - dbh->header_len * sizeof(EntryHeader);
-		fwrite(buf, 1, DATABLOCK, fd);
+//		printf("writing on %zd\n", ftell(fd));
+		assert(fwrite(buf, 1, DATABLOCK, fd) == DATABLOCK);
+//		printf("%d < %d\n", ftell(fd), FILESIZE);
+		fclose(fd);
 	}
-
+*/
 	printf("datafile created\n");
+}
+
+#define IS_SET(var, n) (var[n / 8] & (1 << (n % 8)))
+#define SET(var, n) var[n / 8] |= (1 << (n % 8))
+
+void init_database() {
+	FILE *fd;
+	long i;
+
+	fd = fopen(DATAFILE, "r+");
+	fread(&conf, sizeof(Config), 1, fd);
 	fclose(fd);
+
+	for (i = 3; i < (FILESIZE / DATABLOCK / 8); i++) {
+		if (!IS_SET(conf.bitmap, i))
+			free_blocks = g_list_append(free_blocks, i);
+		else
+			printf("Datablock %d ocupado\n", i);
+	}
 }
 
 int main() {
 	Buffer *b;
 	DBHeader *dbh;
-	Config *conf;
-	FILE *fd = fopen(".datafile", "r");
+	FILE *fd = fopen(DATAFILE, "r");
 	if (!fd) {
 		printf("First run\n");
-		init_database();
+		create_database();
 	}
+	if (fd)
+		fclose(fd);
 
-	fclose(fd);
+	init_database();
+
+	printf("temos %d datablocks livres\n", g_list_length(free_blocks));
 
 	for (int i = 0; i < 256; i++) {
 		b = get_datablock(i);
 		sprintf(b->datablock, "{Object:%d, services: [svc1, svc2]}", i);
-		printf("[insert] %s\n", b->datablock);
 		//TODO: inserir no datablock
+	//	insert(b->datablock, "foostringjson");
 		b->dirty = 1;
 	}
 
@@ -183,13 +222,18 @@ int main() {
 	dbh = &b->datablock[DATABLOCK - 1] - sizeof(DBHeader);
 	printf("header_len = %d\n", dbh->header_len);
 	printf("hit = %d, miss = %d\n", hit, miss);
-	bufmgmt_persist();
 
-	fd = fopen(".datafile", "r");
-	fseek(fd, 0, SEEK_SET);
-	int foo;
-	fread(&foo, 1, sizeof(int), fd);
-	printf("root = %d\n", foo);
+	SET(conf.bitmap, 666);
+	persist();
+
+	b = get_datablock(999);
+	dbh = &b->datablock[DATABLOCK - 1] - sizeof(DBHeader);
+	printf("header_len = %d\n", dbh->header_len);
+	b = get_datablock(0);
+	printf("[select] %s\n", b->datablock);
+	fd = fopen(DATAFILE, "r");
+	fread(&conf, sizeof(Config), 1, fd);
+	printf("root = %d\n", conf.root);
 	fclose(fd);
 	return 0;
 }
